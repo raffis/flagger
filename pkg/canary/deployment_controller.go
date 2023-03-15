407f585cbc77ebceb80a47d579357e48d8d12197
Raffael Sahli <raffael.sahli@doodle.com>
2023-03-15 13:21:47 +0000

    255571310fbd0041e776f0819a4a3c312b070b9f

--- pkg/canary/deployment_controller.go
+++ pkg/canary/deployment_controller.go
@@ -554,12 +554,3 @@ func (c *DeploymentController) appendPrimarySuffixToValuesIfNeeded(labelSelector
 		}
 	}
 }
-
-func contains(slice []string, val string) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
-		}
-	}
-	return false
-}
